0c53a06648f2ff660f1fb883f2f05d4c921beb68
KyungHo An <akh981216@naver.com>
2022-02-11 20:51:26 +0900

    Add files via upload

---
+++ 주말과제/Prob02.java
@@ -0,0 +1,16 @@
+public class Prob02{
+	public static void main(String[] args){
+		int limit =50;
+		boolean prime = false;
+		
+		for(int i=0; i<limit; i++){
+			if(i%2==1){
+				if(i%3==1 || i%3==2){
+					if(i%5 !=0; i%7!=0){
+						System.out.println("i");
+					}
+				}
+			}
+		}
+	}
+}
